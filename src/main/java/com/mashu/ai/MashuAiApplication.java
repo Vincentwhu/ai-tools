4133eca1328207b39311679eb262e20cab14b1a3
Vincentwhu <dgzj050511@163.com>
2025-07-12 20:37:27 +0800

    feat: Initialize project.

---
+++ src/main/java/com/mashu/ai/MashuAiApplication.java
@@ -0,0 +1,13 @@
+package com.mashu.ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MashuAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MashuAiApplication.class, args);
+    }
+
+}
